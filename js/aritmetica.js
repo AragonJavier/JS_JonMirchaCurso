d87a85a849997b127c782c8c2ecd228f81cebf27
Alexander <aragonjavier017@gmail.com>
2021-04-19 18:41:59 -0600

    resolviendo el ultimo ejercicio

--- js/aritmetica.js
+++ js/aritmetica.js
@@ -1,8 +1,10 @@
-export function sumar(a,b){
-  return a+b;
+export function sumar(a, b) {
+  return a + b;
 }
-function restar(a,b){return a-b};
-export const aritmetica={
-  sumar,restar
+function restar(a, b) {
+  return a - b;
+}
+export const aritmetica = {
+  sumar,
+  restar,
 };
-
